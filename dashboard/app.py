776747ca9fdabce22e3d3fa356215647b75cd807
Toghrul <ge75fob@mytum.de>
2021-11-20 19:05:37 +0100

    dashboard updated

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,9 +3,8 @@ import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html
 from apps import index
 from apps import data_visualization
-
-
-
+from apps import data_visualization2
+from apps import isolation_forests
 from apps import anomaly_detection
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -40,6 +39,8 @@ sidebar = html.Div(
             [
                 dbc.NavLink("Home", href="/", active="exact"),
                 dbc.NavLink("Visualize data", href="/data_visualization", active="exact"),
+                dbc.NavLink("Visualize data2", href="/data_visualization2", active="exact"),
+                dbc.NavLink("Isolation forests", href="/isolation_forests", active="exact"),
                 dbc.NavLink("Anomaly detection", href="/anomaly_detection", active="exact"),
             ],
             vertical=True,
@@ -54,6 +55,8 @@ content = html.Div(id="page-content", style=CONTENT_STYLE)
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 data_visualization.get_callbacks(app)
+data_visualization2.get_callbacks(app)
+isolation_forests.get_callbacks(app)
 
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
@@ -61,6 +64,10 @@ def render_page_content(pathname):
         return index.page
     elif pathname == "/data_visualization":
         return data_visualization.layout
+    elif pathname == "/data_visualization2":
+        return data_visualization2.layout
+    elif pathname == "/isolation_forests":
+        return isolation_forests.layout
     elif pathname == "/anomaly_detection":
         return anomaly_detection.layout
     # If the user tries to reach a different page, return a 404 message
---
+++ dashboard/apps/data_visualization2.py
@@ -0,0 +1,41 @@
+import dash
+
+from dash import dcc
+from dash import html
+import plotly.express as px
+from dash.dependencies import Input, Output
+from data_access import data_access as da
+import pandas as pd
+
+
+df = da.read_csv("~/Desktop/hackatum/ZEISS_hacakatum_challenge_dataset.csv")
+sensor_options = []
+for sensor in da.get_unique_sensor_names(df):
+    sensor_options.append({'label':sensor, 'value':sensor})
+
+
+
+
+
+layout = html.Div(id = 'parent', children = [
+    html.H1(id = 'H1', children = 'Interactive visualization', style = {'textAlign':'center',\
+                                            'marginTop':40,'marginBottom':40}),
+
+        dcc.Dropdown( id = 'dropdown',
+        options = sensor_options),
+        dcc.Graph(id = 'bar_plot')
+    ])
+
+
+def get_callbacks(app):
+
+    @app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+                  [Input(component_id='dropdown', component_property= 'value')])
+    def graph_update(dropdown_value):
+        fig = px.scatter(df.loc[df['sensor_name'] == "{}".format(dropdown_value)], x="datetime", y="sensor_value", color="source_id", symbol="region")
+
+        fig.update_layout(title = 'Current Sensor:{} - change of temperature value over time'.format(dropdown_value),
+                          xaxis_title = 'Dates',
+                          yaxis_title = 'Temp'
+                          )
+        return fig
